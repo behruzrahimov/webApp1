7e31efdba670d733f27407800f7a14e99d594561
shoh <behruzrahimov580@gmail.com>
2023-10-22 18:06:03 +0500

    add gitignore and fixed any files

--- src/app/app.server.js
+++ src/app/app.server.js
@@ -1,16 +1,7 @@
 import express from "express";
 import { AppConfig } from "./app.config.js";
-import { connection } from "../db/posgres.js";
-import { createAndInsertUser } from "../db/create-user-table.js";
 
 const app = express();
-
-try {
-  await connection.authenticate();
-  await createAndInsertUser();
-} catch (error) {
-  console.error("Unable to connect to the database:", error);
-}
 
 app.get("/", (req, res) => {
   res.send("Hello, Express with TypeScript!");
